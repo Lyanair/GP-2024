8e486e2dd6c0a1d4b0b3141f140f39d4bb6bf939
Lyanair <Rloughran280@gmail.com>
2025-04-30 17:15:30 +0100

    enemy spawning / damage / animations

--- the-course/Meowrauders/enemy.gd
+++ the-course/Meowrauders/enemy.gd
@@ -3,11 +3,12 @@ extends CharacterBody3D
 
 var player = null
 var state_machine
+var health = 2
 
 const SPEED = 4.0
 const ATTACK_RANGE = 2.0
 
-@export var player_path := ""
+@export var player_path := "/root/World/NavigationRegion3D/Player"
 
 @onready var nav_agent = $NavigationAgent3D
 @onready var anim_tree = $AnimationTree
@@ -24,13 +25,13 @@ func _process(delta):
 	velocity = Vector3.ZERO
 	
 	match state_machine.get_current_node():
-		"Run":
+		"EnemyAnim_Run":
 			# Navigation
 			nav_agent.set_target_position(player.global_transform.origin)
 			var next_nav_point = nav_agent.get_next_path_position()
 			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
 			rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta * 10.0)
-		"Attack":
+		"EnemyAnim_Attack":
 			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 	
 	# Conditions
@@ -49,3 +50,8 @@ func _hit_finished():
 	if global_position.distance_to(player.global_position) < ATTACK_RANGE + 1.0:
 		var dir = global_position.direction_to(player.global_position)
 		player.hit(dir)
+
+func _on_area_3d_body_part_hit(dam):
+	health -= dam
+	if health <= 0:
+		queue_free()
